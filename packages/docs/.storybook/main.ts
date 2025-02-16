c2f8e35b8f8354edf0038e8c7e31caf5247db25a
Vitor Hugo <vitorhugo.pitolsilva@gmail.com>
2025-02-16 17:30:15 -0400

    feat: add subpath to storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -22,5 +22,11 @@ const config: StorybookConfig = {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-design-system/'
+    }
+    return config
+  },
 }
 export default config
