5e8d18d109efa9e0d63d26ebbe679e0940c32419
Vitor Hugo <vitorhugo.pitolsilva@gmail.com>
2025-02-17 00:59:00 -0400

    fix: text input type props

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,17 +1,22 @@
 import { ComponentProps, ComponentRef, forwardRef } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
+import { VariantProps } from '@stitches/react'
 
-export interface TextInputProps extends ComponentProps<typeof Input> {
+export interface TextInputProps
+  extends Omit<ComponentProps<typeof Input>, 'size'> {
   prefix?: string
+  size?: VariantProps<typeof TextInputContainer>['size']
 }
 
 export const TextInput = forwardRef<ComponentRef<typeof Input>, TextInputProps>(
-  ({ prefix, ...props }: TextInputProps, ref) => (
-    <TextInputContainer>
-      {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input ref={ref} {...props} />
-    </TextInputContainer>
-  ),
+  ({ prefix, size, ...props }, ref) => {
+    return (
+      <TextInputContainer size={size}>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
+        <Input ref={ref} {...props} />
+      </TextInputContainer>
+    )
+  },
 )
 
 TextInput.displayName = 'TextInput'
